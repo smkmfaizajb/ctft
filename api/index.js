a68c3e2fdbdc21765ff4344889fe9d505f7be221
smkmfaizajb <smkmajb22@gmail.com>
2025-03-07 21:06:15 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,9 +8,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Pastikan file statis bisa diakses
 app.use(express.static("public"));
 
-const USERS = [
-    { username: "admin", password: "admin1234" }
-];
+const USERS = "admin";
+const PASSWORD = "admin1234";
 const FLAG = "CTFT{345yyy_login_bypass}";
 
 app.get("/", (req, res) => {
@@ -19,18 +18,6 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
     const { username, password } = req.body;
-
-     // Simulasi SQL Query 
-    const fakeSQL = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
-    console.log("Query yang dijalankan:", fakeSQL); // Debug untuk melihat query yang terjadi
-
-    //  payload SQL Injection "membodohi" array USERS
-    const user = USERS.find(u => 
-        (`${u.username}` === username && `${u.password}` === password) || 
-            username.includes("' --") || 
-            username.includes("' OR '1'='1") || 
-            password.includes("' OR '1'='1")       
-    );
     
     if (username === USERNAME && password === PASSWORD) {
         res.send(`<h2 class="success">Yeayy! Login berhasil!</h2><p>Flag: ${FLAG}</p>`);
